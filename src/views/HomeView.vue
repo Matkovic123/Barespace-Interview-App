a93ccd4f0614586d5d1f7bbcb2f9bddf3110b497
tmatkovic <toni.matkovic0@gmail.com>
2024-11-25 00:57:07 +0000

    HomeView improvement

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,14 @@
 
 <template>
   <main>
-    <h1>Home view by Toni</h1>
+    <div class="container">
+      <div class="columns is-centered">
+        <section class="hero">
+          <div class="hero-body">
+            <p class="title">Choose a question</p>
+          </div>
+        </section>
+      </div>
+    </div>
   </main>
 </template>
