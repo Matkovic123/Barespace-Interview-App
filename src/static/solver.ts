01772422869d00ea84487936abaf2c890d607326
tmatkovic <toni.matkovic0@gmail.com>
2024-11-25 00:23:22 +0000

    Improved solution for first task

--- src/static/solver.ts
+++ src/static/solver.ts
@@ -1,12 +1,12 @@
 export const solveTwoSumTwo = (input: number[]): string => {
   const target = 18
-  for (let i = 0; i < input.length - 1; i++) {
-    for (let j = input.length - 1; j > i; j--) {
-      if (input[i] + input[j] === target) {
-        return `Numbers in position ${i} and ${j} summed up give the desired target value. Total time ${totalTime}`
-      }
+  for (let i = 0; i < input.length; i++) {
+    const index = input.indexOf(target - input[i])
+    if (index !== -1) {
+      return `Numbers in position ${i} and ${index} summed up give the desired target value.`
     }
   }
+  // return `Numbers in position ${i} and ${j} summed up give the desired target value. Total time ${totalTime}`
   return 'No matches'
 }
 export const solveCreditCardNumber = () => '222'
